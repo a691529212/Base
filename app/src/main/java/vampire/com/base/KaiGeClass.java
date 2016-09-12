2d5037edf0a1c0ac25c9a92b655fc1da638dea7c
dllo <1096343121@qq.com>
2016-09-12 11:14:25 +0800

    新写的

---
+++ app/src/main/java/vampire/com/base/KaiGeClass.java
@@ -0,0 +1,44 @@
+package vampire.com.base;
+
+/**
+ * code is far away from bug with the animal protecting
+ * <p/>
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃永无BUG！  凯哥 祝你一路顺风
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ */
+public class KaiGeClass {
+
+   private int a ;
+    private String b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+}
